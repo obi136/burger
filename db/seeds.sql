a9d7493cf8288bede29e18d8236de238749e1856
obi136 <obi136@gmail.com>
2018-07-27 01:54:20 -0400

    new commit

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Big Mac", true);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Philly Double Stack Burger", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ("Barbecue Tequila Shot Burger", true);
+
