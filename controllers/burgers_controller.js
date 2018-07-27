496742dfe5186bde35893c4393f88c451935fdb6
obi136 <obi136@gmail.com>
2018-07-27 15:11:52 -0400

    changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,31 +4,29 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res){
-    burger.selectAll(function(data){
+router.get("/", function (req, res) {
+    burger.selectAll(function (data) {
         var hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
+        // console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 
-router.post("/burgers", function(req, res){
-    burger.insertOne(["burger_name"], [req.body.burger_name], function(data){
+router.post("/burgers", function (req, res) {
+    burger.insertOne(["burger_name"], [req.body.burger_name], function (data) {
         res.redirect("/");
     });
 });
 
-router.put("/burgers/:id", function(req, res){
+router.put("/burgers/:id", function (req, res) {
     var condition = "id = " + req.params.id;
-
     console.log("condition", condition);
-
     burger.updateOne({
         devoured: true
-    }, condition, function(data){
-        res.redirect("/");
+    }, condition, function (result) {
+        res.end();
     });
 });
 
